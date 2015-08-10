75775a4bebabe7144304721dd4d665c1818de1e4
Einar Löve <einar.love@gmail.com>
2015-08-10 15:11:48 +0200

    add custom config

---
+++ index.js
@@ -0,0 +1,14 @@
+var path = require('path')
+var stripComments = require('strip-json-comments')
+var fs = require('fs')
+var airbnbConfig = require('./node_modules/airbnb-style/packages/eslint-config-airbnb')
+var merge = require('lodash/object/merge')
+
+var filename = path.join(__dirname, '.eslintrc')
+var data = fs.readFileSync(filename, {encoding: 'utf-8'})
+var dataWithoutComments = stripComments(data)
+var customConfig = JSON.parse(dataWithoutComments)
+
+var config = merge(airbnbConfig, customConfig)
+
+module.exports = config
