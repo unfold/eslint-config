6dc6f10b2a231355f9d739d1427a07348b4e3e5a
einarlove <post@einarlove.com>
2017-02-13 13:39:44 +0100

    Overwride rule to stop enforcing func args comma-dangle

--- index.js
+++ index.js
@@ -22,5 +22,9 @@ module.exports = {
     semi: [2, 'never'],
     'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
     'react/forbid-prop-types': [1, { forbid: ['any'] }],
+
+    // Overwide rule that enforces trailing commas in function arguments
+    // https://github.com/airbnb/javascript/blob/c29e615be497767264b09fa826229f71fd015c38/packages/eslint-config-airbnb-base/rules/errors.js#L4
+    'comma-dangle': [2, 'always-multiline'],
   },
 }
